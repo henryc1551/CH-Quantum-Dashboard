4e85e940c5699610c51f442da754ca69a4477cf4
henryc1551 <henrykleon1551@gmail.com>
2025-09-09 00:34:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,5 @@
-const CACHE = "qdp-cache-v2";
-const PRECACHE = ["/","/index.html","/manifest.webmanifest","/logo-192.png","/logo-512.png","/gfx.js"];
-
-self.addEventListener("install",(e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)));
-});
-
-self.addEventListener("fetch",(e)=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
-  );
-});
-
-self.addEventListener("activate",(e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))
-  );
-});
+const CACHE = "qdp-cache-v1";
+const PRECACHE = ["/","/index.html","/manifest.webmanifest","/logo-192.png","/logo-512.png","/logo-maskable.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE))); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k))))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request))); });
